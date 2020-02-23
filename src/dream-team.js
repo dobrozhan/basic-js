8908d42840c1509ac6eded58f7d5676c8e5e2748
keep_chr <dobrozhan.a@gmail.com>
2020-02-23 18:37:01 +0200

    feat: add dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var firstLetter = [];
+
+  if (Array.isArray(members)) {
+
+  for (let i = 0 ; i < members.length; i++) { 
+      if (typeof members[i] != 'string') { continue; }
+      var result = members[i];
+      var resultTrimmed = result.trim();
+      firstLetter.push(resultTrimmed[0]);
+  }
+
+  firstLetter = firstLetter.map(function (arg) { return arg.toUpperCase();});
+  return firstLetter.sort().join('');
+} else {
+  return false;
+}
 };
