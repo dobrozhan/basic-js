02b5db0427235cefd9b59f86e60b0292fbc17fae
keep_chr <dobrozhan.a@gmail.com>
2020-02-23 09:34:04 +0200

    initial: upload count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,26 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity === '') {
+    return false;
+  } else {
+    let sampleActivityNum = parseFloat(sampleActivity);
+
+    if (sampleActivity === NaN || sampleActivity < 0) {
+    return false;
+    }
+
+    let k = MODERN_ACTIVITY/sampleActivityNum;
+    let n = Math.log(k);
+    let t = 0.693/HALF_LIFE_PERIOD;
+    let time = n/t;
+    let result = Math.ceil(time);
+
+    if ( result < 0 || result == Infinity || isNaN(result) ) {
+      return false;
+    } else {
+      return result;
+    }
+  }
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,20 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+ if (typeof matrix !== 'undefined' || matrix != []) {
+
+  var number = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    
+    for (let j = 0; j < matrix[i].length; j++) {
+
+      if (matrix[i][j] == '^^') {
+        number++;
+      }
+    }
+
+  }
+  return number;
+ } else {
+   return 0;
+ }
 };
