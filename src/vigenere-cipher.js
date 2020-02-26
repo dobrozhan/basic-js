c8bef414698f5837d69a0a65849a13600fe0cb99
keep_chr <dobrozhan.a@gmail.com>
2020-02-26 22:30:19 +0200

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,56 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(flag = true) {
+        this.direct = flag;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (typeof message == 'undefined' || typeof key == 'undefined') {
+            throw new Error();
+            }
+
+            if (typeof message == 'undefined' || typeof key == 'undefined') { throw new Error(); }
+
+            var encryptedChar = '';
+            
+            message = message.toUpperCase();
+            key = key.toUpperCase();
+            
+            for ( let i = 0, j = 0; i < message.length; i++, j++) {
+                if ( j == key.length) { j = 0; }
+                if ( message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90 && key.charCodeAt(j) >= 65 && key.charCodeAt(j) <= 90 ) {
+                    encryptedChar += String.fromCharCode((message.charCodeAt(i) + key.charCodeAt(j) - 130) % 26 + 65);
+                } else { 
+                  encryptedChar += message[i];
+                  j--;
+                }  
+            }
+
+            let encryptedCharRev = encryptedChar.split('').reverse().join('');
+            return this.direct ? encryptedChar : encryptedCharRev;
+    }
+
+    decrypt(encryptedMessage, key) {
+        if(typeof encryptedMessage == 'undefined' || typeof key == 'undefined') {
+            throw new Error();
+        }
+
+        var decryptedStr = '';
+            
+        encryptedMessage = encryptedMessage.toUpperCase();
+        key = key.toUpperCase();
+        
+        for ( let i = 0, j = 0; i < encryptedMessage.length; i++, j++) {
+            if ( j == key.length) { j = 0; }
+            if ( encryptedMessage.charCodeAt(i) >= 65 && encryptedMessage.charCodeAt(i) <= 90 && key.charCodeAt(j) >= 65 && key.charCodeAt(j) <= 90 ) {
+                decryptedStr += String.fromCharCode((encryptedMessage.charCodeAt(i) - key.charCodeAt(j) + 26) % 26 + 65);
+            } else { 
+                decryptedStr += encryptedMessage[i];
+              j--;
+            }  
+        }
+
+        let decryptedStrRev = decryptedStr.split('').reverse().join('');
+        return this.direct ? decryptedStr : decryptedStrRev;
     }
 }
 
