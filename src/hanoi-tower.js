7e2240bbc2c39c700df4a9ff21ca54b98cda7eee
keep_chr <dobrozhan.a@gmail.com>
2020-03-09 12:56:51 +0200

    fix: refactor hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    let turns = Number((2 ** disksNumber - 1).toPrecision(17));
-    let seconds = Number((turns/ (turnsSpeed / 3600)).toPrecision(17));
+    let turns = Number((2 ** disksNumber - 1));
+    let seconds = Number((turns/ (turnsSpeed / 3600)));
 
     let result = {turns: turns, seconds: seconds};
 
